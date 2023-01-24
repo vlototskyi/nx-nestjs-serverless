a431c30a0b698f8ea3bcd451d6905bc31b1c5256
Vitalii Lototskyi <vitalii.lototskyi@oril.co>
2023-01-24 15:55:06 +0200

    Update controller tests

--- apps/serverless/src/app/app.controller.spec.ts
+++ apps/serverless/src/app/app.controller.spec.ts
@@ -15,9 +15,7 @@ describe('AppController', () => {
   describe('getData', () => {
     it('should return "Welcome to serverless!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({
-        message: 'Welcome to serverless!',
-      });
+      expect(appController.getData()).toEqual('Welcome to serverless!');
     });
   });
 });
